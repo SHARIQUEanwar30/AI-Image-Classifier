f701f1edc3ac0ddf9954c2b9665bace4d460a0c3
SHARIQUE ANWAR <shariqueanwar48@gmail.com>
2026-01-13 23:15:39 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from PIL import Image, ImageOps
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.initializers import glorot_uniform
+
+img_size = 48
+
+model = tf.keras.models.load_model("AIGeneratedModel.h5", custom_objects={'GlorotUniform': glorot_uniform()})
+
+model.save("AIGeneratedModel_savedmodel", save_format="tf")
+model = tf.keras.models.load_model("AIGeneratedModel_savedmodel")
+
+
+st.title("AI Image Classifier")       
+        
+img = st.file_uploader("Upload your Image")
+
+if img and st.button("Check"):
+    image = Image.open(img)
+    st.image(img)
+    image = ImageOps.fit(image, (48,48), Image.ANTIALIAS)
+    img_array = img_to_array(image)
+    new_arr = img_array/255
+    test = []
+    test.append(new_arr)
+    test = np.array(test)
+    y = model.predict(test)
+    if y[0] <= 0.5:
+        st.write("The given image is Real.")
+    else:
+        st.write("The given image is AI Generated.")
+    
